c9e5891f41eece99ec17d23858694c77ec31ca02
Jie Deng <jie.deng28@gmail.com>
2020-11-22 13:27:24 -0800

    model dev

--- model_dev/model_dev.py
+++ model_dev/model_dev.py
@@ -6,7 +6,7 @@ Created on Sun Nov 22 13:17:41 2020
 @author: jiedeng
 """
 import numpy as np
-step=np.load('set.000/energy').shape[0]
+step=np.load('set.000/fparam.npy').shape[0]
 print("{0} configs".format(step))
 from subprocess import call
 call("dp test -m /u/home/j/jd848/project-lstixrud/pv_hf_copy/dp-train/extreme_filtered/re7/pv_gpu.pb -d re7 -n {0}".format(step),shell=True)
