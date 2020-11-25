465c2d0551b84ba3f68911be73ee5136c7944959
Jie Deng <jie.deng28@gmail.com>
2020-11-24 22:22:36 -0800

    all

--- model_dev/analysis.py
+++ model_dev/analysis.py
@@ -16,6 +16,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--test_folder","-tf",default='test',help="model test folder")
 parser.add_argument("--recal_foler","-rf",default='recal',help="recal folder")
 parser.add_argument("--model_prefix","-mp",default='re4-re5-re6-re7-re8',help="recal folder")
+parser.add_argument("--energy_cutoff","-ec",default=0.0044,type=float,help="cutoff for energy")
+parser.add_argument("--force_cutoff","-fc",default=0.27,type=float,help="cutoff for force")
 
 args   = parser.parse_args()
 test_folder = args.test_folder
@@ -53,7 +55,7 @@ if len(prefixs) > 1:
     ax[0].plot(nsw+1,es[0],'-',label='re4 nn')
     ax[1].plot(nsw+1,e_diff_per_atom,'-',label='VASP-NN')
     ax[2].plot(nsw+1, rmsd_f[nsw],'-',label='RMSE of force by nn and vasp)')
-    ax[3].plot(nsw+1,mean_std_f[nsw],label=' corr(RMSE, nodel_dev): {:02.4f} / {:02.4f}'.format(corr_this_f,corr_dpgen_f))
+    ax[3].plot(nsw+1,mean_std_f[nsw],label=' corr(RMSE, model_dev): {:02.4f} / {:02.4f}'.format(corr_this_f,corr_dpgen_f))
     ax[0].set_ylabel('E (eV)');
     ax[1].set_ylabel(r'$\Delta E eV/atom$');
     ax[2].set_ylabel('RMSE force (eV/A)');
@@ -69,19 +71,19 @@ if len(prefixs) > 1:
     f_idx=np.where(abs(rmsd_f)>0.27)[0]
     
     cutoffs = np.linspace(0,max(abs(mean_std_f)))
-    ratios_e = []; num_e = []
-    ratios_f = []; num_f = []
+    ratios_e = []; num = []
+    ratios_f = [];  
     for cutoff in cutoffs:
         mod_dev_idx=np.where(abs(mean_std_f)>=cutoff)[0]
         ratios_e.append(len(np.intersect1d(e_idx,mod_dev_idx))/len(mod_dev_idx))
         ratios_f.append(len(np.intersect1d(f_idx,mod_dev_idx))/len(mod_dev_idx))
-        num_e.append(len(mod_dev_idx))
+        num.append(len(mod_dev_idx))
     
-    fig1,ax1 = plt.subplots(1,1,figsize=(6,3),sharex=True)
-    ax1.plot(cutoffs,ratios_e,label='energy>nn average ')
-    ax1.plot(cutoffs,ratios_f,label='force>nn average ')
+    fig1,ax1 = plt.subplots(1,1,figsize=(6,2),sharex=True)
+    ax1.plot(cutoffs,ratios_e,label='rmse e>nn error ')
+    ax1.plot(cutoffs,ratios_f,label='rmse f>nn error ')
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    ax2.plot(cutoffs,num_e,'k')
+    ax2.plot(cutoffs,num,'k')
     ax1.legend()
     ax1.set_xlabel('cutoff')
     ax1.set_ylabel('correct frame fraction')
@@ -89,17 +91,20 @@ if len(prefixs) > 1:
     ax1.grid()
     fig1.savefig('cutoff.png')
     
-    plt.figure()
-    plt.plot(f_idx,e_diff_per_atom[f_idx],'.',label='f > nn {0}'.format(len(f_idx)))
+    fig3,ax3 = plt.subplots(1,1,figsize=(6,2),sharex=True)
+    ax3.plot(f_idx,e_diff_per_atom[f_idx],'.',label='f > nn {0}'.format(len(f_idx)))
     ef_idx = np.intersect1d(e_idx,f_idx)
-    plt.plot(ef_idx,e_diff_per_atom[ef_idx],'.',label='f > nn && e > nn {0}'.format(len(ef_idx)))
-    plt.legend()
-    plt.savefig('idx.png')
-    
-    plt.show()
-    
+    ax3.plot(ef_idx,e_diff_per_atom[ef_idx],'.',label='f > nn && e > nn {0}'.format(len(ef_idx)))
+    ax3.legend()
+    fig3.savefig('idx_efc.png')
+    nsw_efc=np.array(nsw+1).astype(int)[ef_idx]
+    np.savetxt(os.path.join(recal_foler,'nsw_efc'),
+               nsw_efc,
+               header='energy cutoff {0} && force cutoff {1}'.format(args.energy_cutoff, args.force_cutoff))
+    fig3.show() 
     fig.show()
     fig1.show()
+    
 
 else:
     plt.figure()
