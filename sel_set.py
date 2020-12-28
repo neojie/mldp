1ae425e498dac81179d254e42e201330f332432b
Jie Deng <jie.deng28@gmail.com>
2020-12-27 19:36:21 -0800

    sel_set

--- sel_set.py
+++ sel_set.py
@@ -9,10 +9,18 @@ Created on Sun Dec 27 13:30:00 2020
 ### select frames from test test
 import os  
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+#l1=dpdata.System('/Users/jiedeng/Documents/ml/deepmd-kit/my_example/tail_3.dump')
+parser.add_argument("--cutoff","-c",type=float,default=4,help="cutoff of force difference")
+args   = parser.parse_args()
+
 f_test_file = 'mm10'+".f.out"
 f_test = np.loadtxt(f_test_file)
 dif_f_test = np.abs(f_test[:,:3] - f_test[:,3:])
-anomalous_idx = np.unique(np.where(dif_f_test>4)[0]//160)
+cutoff = args.cutoff
+anomalous_idx = np.unique(np.where(dif_f_test>cutoff)[0]//160)
 npys = ['energy.npy', 'box.npy',  'coord.npy' , 'force.npy' , 'fparam.npy' , 'virial.npy']
 
 energy = np.load('set.001/energy.npy')
@@ -22,4 +30,4 @@ os.mkdir('reduced.001')
 for npy in npys:
     tmp = np.load('set.001/'+npy)[idxs]
     np.save('reduced.001/'+npy,tmp)
-np.savetxt('reduced.001/anomalous_idx.txt',anomalous_idx,fmt='%d',header='np.unique(np.where(dif_f_test>4)[0]//160)')
+np.savetxt('reduced.001/anomalous_idx.txt',anomalous_idx,fmt='%d',header='np.unique(np.where(dif_f_test>{0})[0]//160)'.format(cutoff))
