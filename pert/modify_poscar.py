b1d87801d6118012b5d195c8b08f5a50198ad260
Jie Deng <jie.deng28@gmail.com>
2021-01-17 20:03:21 -0800

    pert

---
+++ pert/modify_poscar.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 12:48:28 2021
+
+p0-x: append all newatom position in the end also change the title line
+
+all: append all newatom position in the end also change the title line
+
+@author: jiedeng
+"""
+print('--'*40)
+print('prepare pristine cell as p0.vasp')
+print('assign two indexs of atoms, starting from 0')
+print('specify number of poscar file to be generated')
+print('--'*40)
+import numpy as np
+import copy
+import argparse
+
+parser = argparse.ArgumentParser(description="modify poscar")
+parser.add_argument("-i", type=int, nargs="+", help="index")
+parser.add_argument("-num", type=int, help="num")
+
+args = parser.parse_args()
+
+poscar='p0.vasp'
+datain = open(poscar, 'r')
+idx1 = args.i[0] # python index
+idx2 = args.i[1] #
+num = args.num # to get num atoms in between atom 1 and 2
+
+dat = datain.readlines()
+newdat = copy.copy(dat)
+eles = dat[5].split()
+nums = dat[6].split()
+nums_tmp = np.array(nums).astype(int)
+nums_acc = np.cumsum(nums_tmp)
+
+def judge_ele(idx):
+    for i in range(len(nums_acc)):
+        if idx+1 <= nums_acc[i]:
+            return eles[i]
+
+ele1=judge_ele(idx1)
+ele2=judge_ele(idx2)
+            
+lastlineidx = int(np.array(nums).astype(int).sum())+8
+
+atom1 = dat[8+idx1]
+atom2 = dat[8+idx2]
+
+atom1 = np.array(atom1.split()).astype(float)
+atom2 = np.array(atom2.split()).astype(float)
+
+vec = atom2 - atom1
+
+dv  = vec/(num +1)  # 
+
+newatoms = []
+
+def savedat(newdat,label):
+    fp = open(label,'w')    
+    for line in newdat:
+        fp.write(line)
+    fp.close()
+    
+for i in range(num):# two atom already known
+    idx = i+1
+    newatom = atom1 + dv*idx
+    newatom_str = [str(jj) for jj in newatom]
+    newatom_str = '    '.join(newatom_str)
+    newatom_str = '    ' + newatom_str + '\n'
+    newatoms.append(newatom_str)
+    newdat[8+idx1] = newatom_str  
+    label = 'p'+str(idx)+'.vasp'
+    savedat(newdat, label)
+    
+for j in range(len(newatoms)):
+    idx = lastlineidx + j
+    dat.insert(idx,newatoms[j])
+    
+neweles = copy.copy(eles)
+neweles.append('Xx')
+neweles = '    '.join(neweles)
+neweles = '    ' + neweles + '\n'
+
+newnums = copy.copy(nums)
+newnums.append(str(len(newatoms)))
+newnums = '    '.join(newnums)
+newnums = '    ' + newnums + '\n'
+
+dat[5] = neweles
+dat[6] = newnums
+
+savedat(dat,'all.vasp')
+
+### write log
+
+log = [
+       '# 0.vasp is pristine cell; all.vasp is trajectory \n',
+       '# np.loadtxt(readme).astype(int) extract the (python, starting from 0) index of atoms of interests \n',
+       '# {0} approaches {1} \n'.format(ele1, ele2),
+       "# inspect file by  for i in *.vasp;do echo $i;sed '{0},{1}!d' $i;done \n".format(9+idx1,9+idx1),
+        str(idx1) + '\n',
+        str(idx2) + '\n',
+       ]
+savedat(log,'log')
---
+++ pert/sub_modify_poscar.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 19:35:28 2021
+
+@author: jiedeng
+"""
+
+import glob
+import os
+from shutil import copy
+from subprocess import call
+import argparse
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("--inputpath","-ip",help="input path file")
+parser.add_argument("--inputfile","-if",help="input files for vasp cal")
+#parser.add_argument("--step","-s",default=1,type=int,help="step")
+#parser.add_argument("--range","-r",type=str,help="0-2, means from 0 to 2, default is for all folders")
+parser.add_argument("--run_vasp","-rv",default=True,action='store_false',help="run vasp?, default without input is Yes")
+
+args   = parser.parse_args()
+
+def run(target_path):
+    os.chdir(target_path)
+    sub_file = os.path.join(args.inputfile,'sub_vasp.sh')
+    call("/u/systems/UGE8.6.4/bin/lx-amd64/qsub {0}".format(sub_file), shell=True)
+#    call("bash {0}".format(sub_file), shell=True)
+    os.chdir(cwd)
+    
+pos = glob.glob('p*.vasp')
+cwd = os.getcwd()
+    
+for i in range(len(pos)):
+    os.mkdir(str(i))
+    copy('p'+str(i)+'.vasp',str(i)+'/POSCAR')
+    copy(os.path.join(args.inputfile,'INCAR'),str(i))
+    copy(os.path.join(args.inputfile, 'KPOINTS'),str(i))
+    copy(os.path.join(args.inputfile, 'POTCAR'),str(i))  
+    if args.run_vasp:        
+        run(cwd,str(i))
+    
+
+        
+
+    
