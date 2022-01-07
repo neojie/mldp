b8f1ff951d0db8d5eb00d5cf299591eb899786ef
Jie Deng <jie.deng28@gmail.com>
2022-01-07 10:42:53 -0800

    lmp

--- lmp/pet.py
+++ lmp/pet.py
@@ -6,7 +6,34 @@ Created on Thu Oct 28 08:19:41 2021
 
 # plot pressure, temperature, and energy without entropy (default)
 # output the number of atoms
+# call("grep -A 4 '  FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' OUTCAR | grep 'energy  without entropy' | awk '{print $4}' >e.dat",shell=True)
+# call("grep -A 4 '  FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' OUTCAR | grep 'free  energy   TOTEN' | awk '{print $5}' > etoten.dat",shell=True)
 
+# from subprocess import call
+# call("grep SCALEE OUTCAR | awk '{print $3}' > scalee.dat",shell=True)
+# scalee = np.loadtxt('scalee.dat')
+# if scalee < 1:
+#     print("scalee = {0}< 1".format(scalee))
+#     # call("grep -A 4 '  FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' OUTCAR | grep 'energy  without entropy' | awk '{print $4}' >e.dat",shell=True)
+#     # call("grep -A 4 '  FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' OUTCAR | grep 'free  energy   TOTEN' | awk '{print $5}' > etoten.dat",shell=True)
+#     call("grep ab-initio OSZICAR  | awk '{print $2}'>ab.dat")
+#     call("grep wca OSZICAR  | awk '{print $4}'>wca.dat")
+
+# else:
+#     call("grep 'energy  without entropy' OUTCAR | awk '{print $4}' >e.dat",shell=True)
+# call("grep 'total pressure' OUTCAR | awk '{print $4}' > p.dat",shell=True)
+# call("grep '(temperature' OUTCAR | awk '{print $6}' > t.dat",shell=True)
+# call("grep 'EENTRO' OUTCAR | awk '{print $5}' > eentro.dat",shell=True)
+# call("paste e.dat t.dat > temp",shell=True)
+# call("paste p.dat temp >pet.dat",shell=True)
+# call("rm e.dat t.dat p.dat scalee.dat",shell=True)
+            
+    
+
+# call("paste e.dat t.dat > temp",shell=True)
+# call("paste p.dat temp >pet.dat",shell=True)
+# call("rm e.dat t.dat p.dat scalee.dat",shell=True)
+    
 @author: jiedeng
 """
 import os
@@ -17,17 +44,52 @@ import re
 import numpy as np
 import warnings
 import argparse
+import matplotlib.pyplot as plt
 
 # get nsw_tot
-parser = argparse.ArgumentParser(description="extract P E T V from OUTCAR")
+parser = argparse.ArgumentParser(description="extract quantities from OUTCAR")
 
+helper = 'what to show?  Default:p, ewithout,t\n'\
+        'could be:\n'\
+        'p : pressure\n'\
+        't :  temperature\n'\
+        'vol :  volume\n'\
+        'toten :  free  energy   TOTEN\n'\
+        'ewithout :  energy  without entropy\n'\
+        'scalee :  E(ab-initio) - E(wca)'\
+        'eentro :  EENTRO'
+print(helper)
 parser.add_argument("--beg","-b", type=int,default=0, help="begin from index")
 parser.add_argument("--end","-e", type=int,default=-1, help="end at index")
 parser.add_argument("-p", "--plot", default=True, action='store_false', help="Defualt: plot")
 parser.add_argument("-s", "--save", default=False, action='store_true', help="Defualt: DONOT save plot")
 parser.add_argument("-ep", "--extract_pet", default=True, action='store_false', help="Defualt: plot")
-
+parser.add_argument("--list","-l",default=['p','ewithout','t'], nargs='+',help=helper)
+parser.add_argument("-m", "--marker", type=str,default='', help="marker style supported by Matplotlib")
+parser.add_argument("-ls", "--linestyle", type=str,default='-', help="line style supported by Matplotlib, e.g., None means no lines")
 args = parser.parse_args()
+
+scalee_command = "grep ab-initio OSZICAR | awk '{print $2}' > ab.dat ;"\
+                "grep wca OSZICAR | awk '{print $4}' > wca.dat;"\
+                "paste -d- ab.dat wca.dat | bc > du.dat"
+# print(scalee_command)
+commands = {'p':"grep 'total pressure' OUTCAR | awk '{print $4}' > p.dat",
+            't' :  "grep '(temperature' OUTCAR | awk '{print $6}' > t.dat",
+            'vol' : "grep 'volume of cell' OUTCAR | awk '{print $5}' > vol.dat",
+            'toten' :  "grep 'free  energy   TOTEN' OUTCAR | awk '{print $5}' > toten.dat" ,
+            'ewithout' : "grep 'energy  without entropy' OUTCAR | awk '{print $4}' >e.dat",
+            'scalee' :  scalee_command,
+            'eentro' :  "grep 'EENTRO' OUTCAR | awk '{print $5}' > eentro.dat",
+            }
+
+files = {'p':"p.dat",
+        't' :  "t.dat",
+        'vol' : "vol.dat",
+        'toten' :  "toten.dat" ,
+        'ewithout' : "e.dat",
+        'scalee' :  "du.dat",
+        'eentro' :  "eentro.dat",
+            }
 
 def get_nsw_tot(vasp_dir = '.'):
     if os.path.exists(os.path.join(vasp_dir,'XDATCAR')):
@@ -134,9 +196,6 @@ def blockAverage(datastream, isplot=False, maxBlockSize=0):
 		plt.errorbar(v, blockMean, np.sqrt(blockVar))
 		plt.ylabel('<x>')
 		plt.xlabel('block size')
-
-#		print "<x> = {0:f} +/- {1:f}\n".format(blockMean[-1], np.sqrt(blockVar[-1]))
-
 		plt.tight_layout()
 		plt.show()
 
@@ -159,90 +218,87 @@ except:
     natoms = 'unknown'
     volume = 'unknown'
 
-print("System:", formula,'\t', natoms,'\t', nsw_tot) # can also be done via grep NION
+print("System:", formula,'\t','natoms:', natoms,'\t','nsw:', nsw_tot) # can also be done via grep NION
 
-
-#property_list = []
-#get_pos, get_energy = False, False
-#nsw_i = 0
-#ewithout = []
 
 
 if args.extract_pet:
-    print("**Extracting P E T data**")
+    print("**Extracting data**")
     from subprocess import call
-    call("grep SCALEE OUTCAR | awk '{print $3}' > scalee.dat",shell=True)
-    call(" | awk '{print $3}' > scalee.dat",shell=True)
-    scalee = np.loadtxt('scalee.dat')
-    if scalee < 1:
-        print("scalee = {0}< 1".format(scalee))
-        call("grep -A 4 '  FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' OUTCAR | grep 'energy  without entropy' | awk '{print $4}' >e.dat",shell=True)
-    else:
-        call("grep 'energy  without entropy' OUTCAR | awk '{print $4}' >e.dat",shell=True)
-    call("grep 'total pressure' OUTCAR | awk '{print $4}' > p.dat",shell=True)
-    call("grep '(temperature' OUTCAR | awk '{print $6}' > t.dat",shell=True)
-    call("grep 'EENTRO' OUTCAR | awk '{print $5}' > eentro.dat",shell=True)
-    call("paste e.dat t.dat > temp",shell=True)
-    call("paste p.dat temp >pet.dat",shell=True)
-    call("rm e.dat t.dat p.dat scalee.dat",shell=True)
+    for li in args.list:
+        call(commands[li],shell=True)
+        if li == 'scalee':
+            np.savetxt('du.dat',np.loadtxt('ab.dat')-np.loadtxt('wca.dat'))
 else:
     print("**use existing P E T data pet.dat in the current folder**")
-   
 
-try:
-    pet= np.loadtxt('pet.dat')
-except:
-    ## may be text exist, one corner case that is not considered yet is
-    ## each column may be of different length
-    tmp=np.genfromtxt('pet.dat')
-    pet=tmp[~np.isnan(tmp).any(axis=1)]
-
-eentro = np.loadtxt('eentro.dat')
-pet_be = pet[args.beg:args.end,:]
-ave=np.mean(pet_be,axis=0)
-print("vanilla mean of P E T V")
-print(f'{ave[0]/10}\t{ave[1]}\t{ave[2]}\t{volume}')
-
-#
-p, e, t = pet_be[:,0], pet_be[:,1], pet_be[:,2]
-
-vp, bpvar, bpmean=blockAverage(p)
-ve, bevar, bemean=blockAverage(e)
-vt, btvar, btmean=blockAverage(t)
-
-pmean  = np.round(p.mean(),2); pst  = np.round(p.std(),2);
-emean  = np.round(e.mean(),2); est  = np.round(e.std(),2);
-tmean  = np.round(t.mean(),2); tst  = np.round(t.std(),2);
+res = {}
+means = []
+vares = []
+for li in args.list:
+    file = files[li]
+    res[li] = np.loadtxt(file)
+    vp, var, ave=blockAverage(res[li][args.beg:])
+    means.append(ave[-1])
+    vares.append(var[-1])
 
 # PET order
-print("**P(GPA)/E(eV)/T(K) value and SR, blockaverage**")
-print("%.2f \t %.2f \t %.2f \t %.6f" % (bpmean[-1]/10,bemean[-1],btmean[-1], np.mean(eentro)))
-print("%.2f \t %.2f \t %.2f" % (bpvar[-1]/10,bevar[-1],btvar[-1]))
+print(*args.list,sep='\t')
+print(*means,sep='\t')
+print(*vares,sep='\t')
+
 
 def running_average(x):
     return np.flip(np.flip(x).cumsum()/(np.array(range(0,len(x)))+1))
-if args.plot:
-    import matplotlib.pyplot as plt
-    fig,ax = plt.subplots(3,2,figsize=(8,12),sharex=True)
-    ax[0][0].plot(pet[:,0]/10,label='P (GPa)')
-    ax[0][0].plot(pet[:,0]/10,label='P (GPa)')
-    ax[0][0].plot(args.beg, pet[args.beg,0]/10, 'ko')
-    if args.end<0:
-        end = args.end + nsw_tot
+
+    horizontal_scale = len(args.list)*3
+    fig,ax = plt.subplots(len(args.list),1,figsize=(6,horizontal_scale),sharex=True,sharey=False)
+    if len(args.list) == 1:
+        ax.plot(res[args.list[0]][args.beg:],label=args.list[0],marker=args.marker, alpha=0.6,linestyle=args.linestyle)
+        ax.legend();ax.grid()
     else:
-        end = args.end
-    ax[0][0].plot(end, pet[args.end,0]/10, 'ko')
-    
-    ax[1][0].plot(pet[:,1],label='E (eV)')
-    ax[2][0].plot(pet[:,2],label='T (K)')
-    ax[2][0].set_xlabel("Step")
-    ax[0][0].legend();ax[0][0].grid()
-    ax[1][0].legend();ax[1][0].grid()
-    ax[2][0].legend();ax[2][0].grid()
-    
-    ax[0][1].plot(running_average(pet[:,0]/10));ax[0][1].grid()
-    ax[1][1].plot(running_average(pet[:,1]));ax[1][1].grid()
-    ax[2][1].plot(running_average(pet[:,2]));ax[2][1].grid()
-    if args.save:
-        fig.savefig('pet.png',bbox_inches='tight')
+        for i in range(len(args.list)):
+            ax[i].plot(x[args.starting_i:], res[args.list[i]][args.beg:],label=args.list[i],marker=args.marker, alpha=0.6, linestyle=args.linestyle)
+            ax[i].legend();ax[i].grid()
     plt.show()
+if args.plot:
+    horizontal_scale = len(args.list)*3
+    fig,ax = plt.subplots(len(args.list),1,figsize=(6,horizontal_scale),sharex=True,sharey=False)
+    if len(args.list) == 1:
+        ax.plot(res[args.list[0]][args.beg:],label=args.list[0],marker=args.marker, alpha=0.9,linestyle=args.linestyle)
+        ax.legend();ax.grid()
+    else:
+        for i in range(len(args.list)):
+            ax[i].plot(res[args.list[i]][args.beg:],label=args.list[i],marker=args.marker, alpha=0.9, linestyle=args.linestyle)
+            ax[i].legend();ax[i].grid()
+    plt.show()
+
+if args.save:
+    fig.savefig('pet.png',bbox_inches='tight')
+plt.show()
+
+# if args.plot:
+#     # import matplotlib.pyplot as plt
+#     fig,ax = plt.subplots(3,2,figsize=(8,12),sharex=True)
+#     ax[0][0].plot(pet[:,0]/10,label='P (GPa)')
+#     ax[0][0].plot(pet[:,0]/10,label='P (GPa)')
+#     ax[0][0].plot(args.beg, pet[args.beg,0]/10, 'ko')
+#     if args.end<0:
+#         end = args.end + nsw_tot
+#     else:
+#         end = args.end
+#     ax[0][0].plot(end, pet[args.end,0]/10, 'ko')
+    
+#     ax[1][0].plot(pet[:,1],label='E (eV)')
+#     ax[2][0].plot(pet[:,2],label='T (K)')
+#     ax[2][0].set_xlabel("Step")
+#     ax[0][0].legend();ax[0][0].grid()
+#     ax[1][0].legend();ax[1][0].grid()
+#     ax[2][0].legend();ax[2][0].grid()
+    
+#     ax[0][1].plot(running_average(pet[:,0]/10));ax[0][1].grid()
+#     ax[1][1].plot(running_average(pet[:,1]));ax[1][1].grid()
+#     ax[2][1].plot(running_average(pet[:,2]));ax[2][1].grid()
+#     if args.save:
+#         fig.savefig('pet.png',bbox_inches='tight')
+#     plt.show()
